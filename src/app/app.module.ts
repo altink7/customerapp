ce1274c4266a88f38cf6186110b4c45ac4362dde
Altin <kelmendialtin48@gmail.com>
2022-09-23 18:42:09 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
